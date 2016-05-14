a29d8b5c1192f7694721b3feebfc91af0aab96cb
Lukas Pirl <git@lukas-pirl.de>
2016-05-14 20:15:38 +0000

    install_tools.sh: made sure we always have the latest versions installed

--- installation/install_tools.sh
+++ installation/install_tools.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+packages=""
+
 # for the filename command
-tools/install_package wcstools
+packages+=" wcstools"
 
 # for the realpath command
-tools/install_package realpath
+packages+=" realpath"
 
 # for virtual machine creation
 # see the tutorial http://serverascode.com/2014/03/17/trusty-libvirt.html
-tools/install_package bzr
+packages+=" bzr"
 
 # for the virtual machine manager
-tools/install_package virt-manager
+packages+=" virt-manager"
 
 # for pip
-tools/install_package python-pip
+packages+=" python-pip"
+
+tools/install_latest_package $packages
 
 # install Jinja2 for template instanciation
 if ! python -c "import jinja2"
 then
 	sudo pip install --upgrade Jinja2
 fi
-
-
