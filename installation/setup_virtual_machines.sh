68b463d6f72fc35564d863ba67d730a3d6ec8fd1
Lukas Pirl <git@lukas-pirl.de>
2016-05-14 21:56:47 +0000

    set global sudo password

--- installation/setup_virtual_machines.sh
+++ installation/setup_virtual_machines.sh
@@ -3,5 +3,5 @@
 # execute playbooks on ansible hosts
 source config/installation_configuration.sh
 
-ansible-playbook $ansible_playbooks_directory/openstack_installation.yml --ask-sudo-pass || exit 1
+ansible-playbook $ansible_playbooks_directory/openstack_installation.yml || exit 1
 
