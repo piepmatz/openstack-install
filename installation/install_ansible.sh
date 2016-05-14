dccfa7e76604ac04c1f0b559257d1bda171a18ae
Lukas Pirl <git@lukas-pirl.de>
2016-05-14 20:15:11 +0000

    made sure we always have the latest version of Ansible installed

--- installation/install_ansible.sh
+++ installation/install_ansible.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-# if ansible is not installed, yet
-if ! type ansible >> /dev/null
-then
-	# install ansible
-	sudo apt-add-repository -y ppa:ansible/ansible
-	sudo apt-get -y update
-	sudo apt-get -y install software-properties-common ansible
-fi
+# instantly fail if any command fails
+set -e
+
+# we do not check if ansible is already installed so we
+# get the latest version
+
+sudo apt-add-repository -y ppa:ansible/ansible 2> /dev/null
+sudo apt-get -y update > /dev/null
+tools/install_latest_package software-properties-common ansible
 
